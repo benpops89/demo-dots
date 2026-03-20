6e6bbc8b8a7d9c2ad14332a79d29137938db1c23
Ben Poppy <me@benpoppy.dev>
2026-03-20 12:12:09 +0000

    Add catppuccin macchiato theme with transparent background

--- .config/nvim/lua/config/lazyvim.lua
+++ .config/nvim/lua/config/lazyvim.lua
@@ -1,3 +1,13 @@
 vim.opt.number = true
 vim.opt.relativenumber = true
 vim.opt.signcolumn = "yes"
+
+vim.opt.termguicolors = true
+vim.opt.background = "dark"
+
+require("catppuccin").setup({
+  transparent_background = true,
+  flavour = "macchiato",
+})
+
+vim.cmd("colorscheme catppuccin")
--- .config/nvim/lua/plugins/lazyvim.lua
+++ .config/nvim/lua/plugins/lazyvim.lua
@@ -1,1 +1,3 @@
-return {}
+return {
+  { "catppuccin/nvim", name = "catppuccin", priority = 1000 },
+}
